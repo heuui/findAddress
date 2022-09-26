c1038c372682cfdf4f143ecd44dc4a4e70e614c8
heuui <henni.haavistola@gmail.com>
2022-09-26 15:50:09 +0300

    valmis tehtävä

--- App.js
+++ App.js
@@ -1,10 +1,59 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useState } from 'react';
+import { Button, StyleSheet, Text, Dimensions, TextInput, View } from 'react-native';
+import MapView, { Marker } from 'react-native-maps';
 
 export default function App() {
+
+  const [address, setAddress] = useState('');
+  const [markerLat, setMarkerLat] = useState(0.0);
+  const [markerLong, setMarkerLong] = useState(0.0);
+  const [regionLat, setRegionLat] = useState(0.0);
+  const [regionLong, setRegionLong] = useState(0.0);
+  const [data, setData] = ([]);
+
+
+  const fetchCoordinates = () => {
+    fetch(`http://www.mapquestapi.com/geocoding/v1/address?key=cfh4afLRpNqrdCDwa9cgPvwgTjsOcmHG&location=${address}`)
+    .then(response => response.json())
+    .then(data => {
+      const lat = data.results[0].locations[0].latLng.lat;
+      const long = data.results[0].locations[0].latLng.lng;
+      setMarkerLat(lat);
+      setRegionLat(lat);
+      setMarkerLong(long);
+      setRegionLong(long);
+    })
+    .catch(error => {
+      console.log(error)
+    });
+  }
+
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <MapView 
+        style={styles.map}
+        region={{
+        latitude: regionLat,
+        longitude: regionLong,
+        latitudeDelta: 0.0322,
+        longitudeDelta: 0.0221,
+      }}>
+      <Marker coordinate={{
+        latitude: markerLat,
+        longitude: markerLong}}
+        title={address} 
+      />
+      </MapView>
+      <TextInput 
+        style={styles.input}
+        onChangeText={text => setAddress(text)} value={address}
+      />
+      <Button 
+        title="Show"
+        onPress={fetchCoordinates}
+      />
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +65,17 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+    marginBottom: 30,
+  },
+  input: {
+    marginTop: 20,
+    width: 200,
+    borderColor: 'gray',
+    borderWidth: 1,
+  },
+  map: {
+    flex: 1,
+    width: Dimensions.get('window').width,
+    height: Dimensions.get('window').height,
   },
 });
